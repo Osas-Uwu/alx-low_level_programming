03fe39966a55b9faf06c35941cf1de5ca840a68f
jojobutercup <jojobutercup@gmail.com>
2022-06-30 16:27:50 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include<Stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ *  * main - Entry point
  *
- * Return: Always 0 (success)
+ *  * Return: Always 0 (Success)
  */
 int main(void)
 {
