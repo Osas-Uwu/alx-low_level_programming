ed55537d633d267fb482bed20e7526070989f292
jojobutercup <jojobutercup@gmail.com>
2022-07-18 08:31:56 -0700

    commit 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,23 @@
-nclude "main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - ills memory with a constant byte
- * @memset: source
- * @dest: destination
+ * _memset - function fills the first n bytes of the me
+ * mory area pointed to by s with the constant byte b
  *
+ * @s: pointer to memory
+ * @b: constant byte to fill memory
+ * @n: size of byte
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
