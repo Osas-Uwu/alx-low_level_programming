2c43fd38101a797c510cabba8dd95c04f27014f1
jojobutercup <jojobutercup@gmail.com>
2022-07-25 13:32:46 -0700

    commit 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of char, initializing a specific char.
+ *
+ * @size: size of array
+ * @c: char to contain the array
+ *
+ * Return: (s), contained array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
+}
