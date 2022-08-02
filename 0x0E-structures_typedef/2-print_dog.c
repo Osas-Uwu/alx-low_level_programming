4ca6fa9a45f652fb2319aac0f4a251f0e9e48217
jojobutercup <jojobutercup@gmail.com>
2022-08-02 12:56:08 -0700

    commit 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-
 /**
  * print_dog - printed struct dog
  * @d: dog struct
@@ -9,34 +8,20 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 
 	if (d->name == NULL)
-	{
 		printf("Name: (nil)\n");
-	}
 	else
-	{
 		printf("Name: %s\n", d->name);
-	}
 
 	if (d->age < 0)
-	{
 		printf("age: (nil)\n");
-	}
 	else
-	{
 		printf("age: %f\n", d->age);
-	}
 
 	if (d->owner == NULL)
-	{
 		printf("owner: (nil)\n");
-	}
 	else
-	{
 		printf("owner: %s\n", d->owner);
-	}
 }
