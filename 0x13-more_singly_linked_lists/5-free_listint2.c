a16cc28d16fc04a9b0a164ce843d0bd954628e2e
jojobutercup <jojobutercup@gmail.com>
2022-08-17 06:07:35 -0700

    commit 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,22 +2,17 @@
 /**
  * free_listint2 - free a list
  * @head: first node of list
- * Return: Void
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head == NULL)
-	{
-		ptr = *head;
+	if (head == NULL)
 		return;
-	}
-
 	while (*head != NULL)
 	{
-		ptr = *head;
-		*head = ptr->next;
-		free(ptr);
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
 	}
 }
